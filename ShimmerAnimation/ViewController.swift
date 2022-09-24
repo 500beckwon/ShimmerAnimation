1cdaf2b257f2668da4e97467f59e417dbe6bbc88
500beckwon <kjsx60@gmail.com>
2022-09-24 20:42:37 +0900

    Initial Commit

---
+++ ShimmerAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShimmerAnimation
+//
+//  Created by ByungHoon Ann on 2022/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
